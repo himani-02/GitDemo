6461cf66d67e38c2fdf2c1f4b156626585c76329
Your Name <Himani>
2018-02-25 12:12:56 +0530

    Add comment

--- app/src/main/java/com/example/himan/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/himan/gitdemo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"Git",Toast.LENGTH_SHORT).show();
+        //Master branch
     }
 }
